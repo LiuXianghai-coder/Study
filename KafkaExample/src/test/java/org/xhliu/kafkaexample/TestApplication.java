09b7db6c7aafad975c2dde36a7cb00d777e83df4
XianghaiLiu <1900327840@qq.com>
2022-01-01 21:54:22 +0800

    Add ProtoBuf Example

--- KafkaExample/src/test/java/org/xhliu/kafkaexample/TestApplication.java
+++ KafkaExample/src/test/java/org/xhliu/kafkaexample/TestApplication.java
@@ -1,17 +1,31 @@
 package org.xhliu.kafkaexample;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.xhliu.kafkaexample.vo.Message;
+import org.xhliu.kafkaexample.protos.gen.AddressBook;
+import org.xhliu.kafkaexample.protos.gen.Person;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class TestApplication {
-    public static void main(String[] args) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
+    public static void main(String[] args) throws Exception {
+        Person person = Person.newBuilder()
+                .setId(1)
+                .setEmail("123456789@gmail.com")
+                .setName("xhliu")
+                .addPhones(Person.PhoneNumber.newBuilder().setNumber("123456789").build())
+                .build();
 
-        Message message = new Message(1, "This is a Message");
-        String json = mapper.writer().writeValueAsString(message);
-        System.out.println(json);
+        AddressBook addressBook = AddressBook.newBuilder().addPeople(person).build();
+        try (
+                FileOutputStream out = new FileOutputStream("/tmp/addressBook.obj");
+                FileInputStream in = new FileInputStream("/tmp/addressBook.obj")
+            ){
+            addressBook.writeTo(out);
+//            System.out.println("序列化 AddressBook 对象成功");
 
-        Message value = mapper.readValue(json, Message.class);
+            AddressBook obj = AddressBook.parseFrom(in);
+            System.out.println("反序列化之后的对象：");
+            System.out.println(obj.getPeople(0));
+        }
     }
 }
