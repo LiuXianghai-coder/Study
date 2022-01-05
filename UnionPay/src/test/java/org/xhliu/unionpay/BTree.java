869cc012655cb143a66073a0e26b7884c4017d2d
XianghaiLiu <1900327840@qq.com>
2022-01-05 22:54:15 +0800

    Modify B-Tree Implements

--- UnionPay/src/test/java/org/xhliu/unionpay/BTree.java
+++ UnionPay/src/test/java/org/xhliu/unionpay/BTree.java
@@ -38,7 +38,7 @@ public class BTree<Key extends Comparable<Key>, Value> {
         }
 
         M = m;
-        root = new Node(1, M);
+        root = new Node(0, M);
     }
 
     public int size() {
@@ -54,25 +54,25 @@ public class BTree<Key extends Comparable<Key>, Value> {
             throw new IllegalArgumentException("key 不能为 null");
         }
 
-        return search(root, key, height);
+        return search(root, key);
     }
 
     @SuppressWarnings("unchecked")
-    public Value search(Node x, Key key, int h) {
+    public Value search(Node x, Key key) {
         if (x == null) return null;
 
         Entry[] entries = x.children;
-        for (int i = 0; i < x.m; ++i) {
+        for (int i = 1; i <= x.m; ++i) {
             if (eq(entries[i].key, key))
                 return (Value) entries[i].value;
         }
 
-        for (int i = 1; i < x.m; ++i) {
+        for (int i = 1; i <= x.m; ++i) {
             if (less(entries[i - 1].key, key) && less(key, entries[i].key))
-                return search(entries[i - 1].next, key, h + 1);
+                return search(entries[i - 1].next, key);
         }
 
-        return null;
+        return search(entries[x.m].next, key);
     }
 
     public void put(Key key, Value value) {
@@ -93,7 +93,7 @@ public class BTree<Key extends Comparable<Key>, Value> {
         Entry t = new Entry(key, value, null);
         Entry[] entries = x.children;
         if (h == height) {
-            for (idx = 1; idx < x.m; ++idx) {
+            for (idx = 1; idx <= x.m; ++idx) {
                 if (eq(key, entries[idx].key)) {
                     entries[idx].value = value;
                     size--;
@@ -103,7 +103,7 @@ public class BTree<Key extends Comparable<Key>, Value> {
                 if (less(key, entries[idx].key)) break;
             }
         } else {
-            for (idx = 1; idx < x.m; ++idx) {
+            for (idx = 1; idx <= x.m; ++idx) {
                 if (eq(key, entries[idx].key)) {
                     entries[idx].value = value;
                     size--;
@@ -115,30 +115,222 @@ public class BTree<Key extends Comparable<Key>, Value> {
                 Node u = insert(entries[idx - 1].next, key, value, h + 1);
                 if (u == null) return null;
 
-                t.key = u.children[1].key;
-                t.value = u.children[1].value;
-                t.next = u;
+                for (idx = 1; idx <= x.m; ++idx)
+                    if (less(u.children[1].key, x.children[idx].key)) break;
+
+                x.children[idx - 1].next = u.children[0].next;
+                t = u.children[1];
                 break;
             }
 
-            if (idx == x.m) {
-                Node u = insert(entries[x.m - 1].next, key, value, h + 1);
+            if (idx > x.m) {
+                Node u = insert(entries[x.m].next, key, value, h + 1);
                 if (u == null) return null;
 
-                t.key = u.children[1].key;
-                t.value = u.children[1].value;
-                t.next = u;
+                for (idx = 1; idx <= x.m; ++idx)
+                    if (less(u.children[1].key, x.children[idx].key)) break;
+
+                x.children[idx - 1].next = u.children[0].next;
+                t = u.children[1];
             }
         }
 
-        for (int i = M; i > idx; --i)
-            x.children[i] = x.children[i - 1];
+        if (M - idx >= 0) System.arraycopy(x.children, idx, x.children, idx + 1, M - idx);
 
         x.children[idx] = t;
         x.m++;
 
         if (x.m < M) return null;
         return split(x);
+    }
+
+    public Entry delete(Key key) {
+        Entry entry = delete(null, root, key, 0);
+        size--;
+
+        return entry;
+    }
+
+    @SuppressWarnings("unchecked")
+    private Entry delete(Node parent, Node cur, Key key, int h) {
+        Entry entry;
+        int idx;
+        // 待删除的节点是一个叶子节点，直接进行删除
+        if (h == height) {
+            for (idx = 1; idx <= cur.m; ++idx)
+                if (eq(cur.children[idx].key, key)) break;
+
+            // 如果当前叶子节点不存在这样的键值对元素，则跳过
+            if (idx > cur.m) return null;
+            entry = cur.children[idx];
+
+            // 移动节点元素列表，删除元素
+            if (cur.m + 1 - idx >= 0)
+                System.arraycopy(cur.children, idx + 1, cur.children, idx, cur.m + 1 - idx);
+            cur.m--;
+
+            // 如果删除节点之后该节点不满足条件，需要对该节点进行重平衡
+        } else {
+            /*
+                待删除的节点不是叶子节点，那么需要从元素的所有后继节点中找到最小的元素（或者从前驱节点中找到最大的元素）
+                覆盖掉当前 key 所在的元素，然后在后继节点中删除该最小元素
+             */
+            for (idx = 1; idx <= cur.m; ++idx) {
+                if (eq(cur.children[idx].key, key)) break;
+                // 当前节点在该节点的后继节点中，递归进行删除
+                if (less(key, cur.children[idx].key))
+                    return delete(cur, cur.children[idx - 1].next, key, h + 1);
+            }
+
+            // idx > cur.m 说明待删除的元素在节点最后的一个元素区间内，同样地，通过递归的方式进行删除
+            if (idx > cur.m)
+                return delete(cur, cur.children[cur.m].next, key, h + 1);
+
+            // 删除内部节点元素
+            Entry min = min(cur);
+            cur.children[idx].key = min.key;
+            cur.children[idx].value = min.value;
+
+            entry = delete(cur, cur.children[idx].next, (Key) min.key, h + 1);
+        }
+
+        if (cur.m < M / 2) reBalance(parent, cur, h);
+
+        return entry;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void reBalance(Node parent, Node cur, int h) {
+        int idx;
+        Entry[] children = parent.children;
+        for (idx = 1; idx <= parent.m; ++idx)
+            if (less(cur.children[cur.m].key, children[idx].key))
+                break;
+        idx -= 1;
+
+        // 找到该叶子节点的左右兄弟节点
+        Node left = null, right = null;
+        if (idx > 0) left = children[idx - 1].next;
+        if (idx < parent.m) right = children[idx + 1].next;
+
+        if (left == null && right == null) return;
+
+        /*
+            左子节点存在多余的元素，从左子节点借用一个元素，使得节点最终满足 B 树的条件
+         */
+        if (left != null && left.m > M / 2) {
+            // 移动当前节点的元素，为新加入的元素腾出位置
+            for (int i = cur.m + 1; i > idx; --i) {
+                if (cur.children[i] == null)
+                    cur.children[i] = new Entry(null, null, null);
+                cur.children[i].key = cur.children[i - 1].key;
+                cur.children[i].value = cur.children[i - 1].value;
+            }
+
+            // 复制属性到当前节点的第一个元素（从 1 开始计数）
+            cur.children[1].key = children[idx].key;
+            cur.children[1].value = children[idx].value;
+            cur.m++;
+
+            // 将从左子节点借用到的元素的属性复制到父节点的分隔元素，使得树最终是有序的
+            children[idx].key = left.children[left.m].key;
+            children[idx].value = left.children[left.m].value;
+
+            // 删除左子节点的最大元素
+            left.children[left.m] = null;
+            left.m--;
+            return;
+        }
+
+        /*
+            右子节点存在多余的元素，因此从右子节点借用一个元素到父节点的分隔节点，
+            同时将原有的旧分隔元素移动到到当前的节点，使得它维持 B 树的结构
+        */
+        if (right != null && right.m > M / 2) {
+            ++cur.m;
+            // 如果此时这个位置的对象未实例化，那么首先实例化该位置的对象
+            if (cur.children[cur.m] == null)
+                cur.children[cur.m] = new Entry(null, null, null);
+
+            // 单纯地复制属性到当前的节点，如果使用引用复制的话会导致出现冗余的链接，甚至出现环
+            cur.children[cur.m].key = children[idx + 1].key;
+            cur.children[cur.m].value = children[idx + 1].value;
+            cur.children[cur.m].next = right.children[0].next;
+
+            // 更新父节点的分隔元素
+            children[idx + 1].key = right.children[1].key;
+            children[idx + 1].value = right.children[1].value;
+            right.children[0].next = right.children[1].next;
+
+            // 由于右子节点被借用了一个元素，因此需要移动右子节点的元素列表使得其依旧是有序的
+            if (right.m >= 0)
+                System.arraycopy(right.children, 2, right.children, 1, right.m);
+            right.m--;
+            return;
+        }
+
+        /*
+            由于左右兄弟节点都不存在多余的元素，因此需要从父节点借用一个元素，合并成为一个节点
+            一般会优先选择左兄弟节点作为合并后的节点，因为这样就不需要移动前半部分元素
+         */
+        if (left != null) {
+            // 首先将父节点的分隔节点复制到当前节点的末尾，由于这个位置可能未实例化，因此首先实例化
+            ++left.m;
+            if (left.children[left.m] == null)
+                left.children[left.m] = new Entry(null, null, null);
+
+            left.children[left.m].key = children[idx].key;
+            left.children[left.m].value = children[idx].value;
+            ++left.m;
+            // 复制父节点的分隔节点结束。。。。
+
+            // 再将当前节点的所有元素复制到左兄弟节点，由于位置 0 是一个哨兵元素，因此从元素 1 开始进行复制
+            for (int i = 1; i <= cur.m; ++i)
+                left.children[++left.m] = cur.children[i];
+
+            // 合并之后，会出现一条多余的链接，这个链接是多余的
+            children[idx].next = null;
+            // 删除父节点的分隔元素之后，移动父节点的分隔元素列表，使得原有的父节点的元素依旧是有序的
+            if (parent.m + 1 - idx >= 0)
+                System.arraycopy(children, idx + 1, children, idx, parent.m + 1 - idx);
+            parent.m--; // 注意这里父节点的元素个数经过合并之后已经减少了
+            return;
+        }
+
+        /*
+            由于不存在左兄弟节点，因此只能选择右边的兄弟节点，将右兄弟节点的元素复制到当前节点
+            Hint：根据 B 树的定义，不可能存在既不含有左兄弟节点，也不含有右兄弟节点的叶子节点
+         */
+        ++cur.m;
+        if (cur.children[cur.m] == null)
+            cur.children[cur.m] = new Entry(null, null, null);
+        cur.children[cur.m].key = children[idx + 1].key;
+        cur.children[cur.m].value= children[idx + 1].value;
+        cur.children[cur.m].next = right.children[0].next;
+        children[idx + 1].next = null;
+
+        for (int i = 1; i <= right.m; ++i)
+            cur.children[++cur.m] = right.children[i];
+        // 调整父节点的元素列表
+        if (parent.m + 1 - (idx + 1) >= 0)
+            System.arraycopy(children, idx + 1 + 1, children, idx + 1, parent.m + 1 - (idx + 1));
+        parent.m--; // 这里又有一个坑 :(
+
+        /*
+            注意这里根节点的变化，只有在树的高度为 1 并且这一层的所有节点都合并成为一个节点时，
+            才能替换掉根节点，此时树的高度 -1
+         */
+        if (parent.m == 0 && h == 1) {
+            root = cur;
+            height--;
+        }
+    }
+
+    private Entry min(Node x) {
+        if (x.children[1].next == null)
+            return x.children[1];
+
+        return min(x.children[1].next);
     }
 
     private Node split(Node x) {
@@ -152,6 +344,10 @@ public class BTree<Key extends Comparable<Key>, Value> {
 
         Node p = new Node(1, M);
         Entry mid = x.children[M / 2 + 1];
+
+        // 注意这里的中间节点的 next 链接需要保存到 t 的哨兵元素节点
+        t.children[0].next = x.children[M / 2 + 1].next;
+
         x.children[M / 2 + 1] = null; // clear mid
         p.children[0].next = x;
         p.children[1] = new Entry(mid.key, mid.value, t);
@@ -177,55 +373,40 @@ public class BTree<Key extends Comparable<Key>, Value> {
     }
 
     private String toString(Node h, int ht, String indent) {
+        if (h == null) return "";
         StringBuilder s = new StringBuilder();
         Entry[] children = h.children;
 
         if (ht == 0) {
-            for (int j = 0; j < h.m; j++) {
+            for (int j = 0; j <= h.m; j++) {
+                if (children[j] == null) continue;
                 s.append(indent).append(children[j].key).append(" ").append(children[j].value).append("\n");
             }
-        }
-        else {
-            for (int j = 0; j < h.m; j++) {
+        } else {
+            for (int j = 0; j <= h.m; j++) {
+                if (children[j] == null) continue;
                 if (j > 0) s.append(indent).append("(").append(children[j].key).append(")\n");
-                s.append(toString(children[j].next, ht-1, indent + "     "));
+                assert children[j] != null;
+                s.append(toString(children[j].next, ht - 1, indent + "     "));
             }
         }
         return s.toString();
     }
 
     public static void main(String[] args) {
-        BTree<String, String> st = new BTree<>(5);
+        BTree<Integer, Integer> st = new BTree<>(5);
 
-        st.put("www.cs.princeton.edu", "128.112.136.12");
-        st.put("www.cs.princeton.edu", "128.112.136.11");
-        st.put("www.princeton.edu",    "128.112.128.15");
-        st.put("www.yale.edu",         "130.132.143.21");
-        st.put("www.simpsons.com",     "209.052.165.60");
-        st.put("www.apple.com",        "17.112.152.32");
-        st.put("www.amazon.com",       "207.171.182.16");
-        st.put("www.ebay.com",         "66.135.192.87");
-        st.put("www.cnn.com",          "64.236.16.20");
-        st.put("www.google.com",       "216.239.41.99");
-        st.put("www.nytimes.com",      "199.239.136.200");
-        st.put("www.microsoft.com",    "207.126.99.140");
-        st.put("www.dell.com",         "143.166.224.230");
-        st.put("www.slashdot.org",     "66.35.250.151");
-        st.put("www.espn.com",         "199.181.135.201");
-        st.put("www.weather.com",      "63.111.66.11");
-        st.put("www.yahoo.com",        "216.109.118.65");
+        for (int i = 1; i <= 22; ++i)
+            st.put(i, i);
 
+        st.delete(1);
+        st.delete(6);
+        st.delete(5);
+        st.delete(4);
+        st.delete(12);
 
-        System.out.println("cs.princeton.edu:  " + st.get("www.cs.princeton.edu"));
-        System.out.println("hardvardsucks.com: " + st.get("www.harvardsucks.com"));
-        System.out.println("simpsons.com:      " + st.get("www.simpsons.com"));
-        System.out.println("apple.com:         " + st.get("www.apple.com"));
-        System.out.println("ebay.com:          " + st.get("www.ebay.com"));
-        System.out.println("dell.com:          " + st.get("www.dell.com"));
-        System.out.println();
-
-        System.out.println("size:    " + st.size());
-        System.out.println("height:  " + st.height());
+        System.out.println("size: " + st.size());
+        System.out.println("height: " + st.height());
         System.out.println(st);
         System.out.println();
     }
