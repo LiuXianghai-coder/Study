fa7ad8a0474e7864fb53722f609fa1ea401513d3
XianghaiLiu <1900327840@qq.com>
2022-01-10 22:39:30 +0800

    Add ACS Project

---
+++ ACS/src/test/java/org/xhliu/acs/AcsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xhliu.acs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
